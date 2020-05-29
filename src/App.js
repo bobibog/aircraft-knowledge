d2a4c78dadc8cd978f927a074caaf6b5bac10077
Boban Bogojević <boban.bogojevic@gmail.com>
2020-05-29 17:40:19 +0200

    Project created with create-react-app.

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  //const person = event.target.value;
+  
+  return (
+    
+    <div className="App">    
+      
+    </div>
+  );
+}
+
+export default App;
