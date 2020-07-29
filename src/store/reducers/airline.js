fdbfcb07cabe8bcc0d3d587a80e81838a06fc2e7
Boban Bogojević <boban.bogojevic@gmail.com>
2020-07-29 16:33:08 +0200

    Airline reducer refactored (made leaner).

--- src/store/reducers/airline.js
+++ src/store/reducers/airline.js
@@ -1,5 +1,6 @@
 import * as actionTypes from '../actions/actionTypes';
 import {rowsPerPageDefault} from '../../shared/staticData';
+import {updateObject} from '../../shared/utility';
 
 const initialState = {
     airlines: null,
@@ -10,38 +11,43 @@ const initialState = {
     airlinesPage: 0
 };
 
+const setAirlinesOffsetLimit = (state, action) => {
+    return updateObject(state, {
+        airlinesOffset: action.offset,
+        airlinesLimit: action.limit
+    });
+};
+const setAirlinesPage = (state, action) => {
+    return updateObject(state, {
+        airlinesPage: action.page
+    });
+};
+const fetchAirlinesStart = (state, action) => {
+    return updateObject(state, {
+        airlinesLoading: true
+    });
+};
+const fetchAirlinesSuccess = (state, action) => {
+    return updateObject(state, {
+        airlines: action.airlines,
+        airlinesCount: action.airlinesCount,
+        airlinesLoading: false
+    });
+};
+const fetchAirlinesFail = (state, action) => {
+    return updateObject(state, {
+        airlinesLoading: false
+    });
+};
+
 const reducer = (state = initialState, action) => {
     switch (action.type) {
-        case actionTypes.SET_AIRLINES_OFFSET_LIMIT:
-            return {
-                ...state,
-                airlinesOffset: action.offset,
-                airlinesLimit: action.limit
-            };
-        case actionTypes.SET_AIRLINES_PAGE:
-            return {
-                ...state,
-                airlinesPage: action.page
-            };
-        case actionTypes.FETCH_AIRLINES_START:
-            return {
-                ...state,
-                airlinesLoading: true
-            };
-        case actionTypes.FETCH_AIRLINES_SUCCESS:
-            return {
-                ...state,
-                airlines: action.airlines,
-                airlinesCount: action.airlinesCount,
-                airlinesLoading: false
-            };
-        case actionTypes.FETCH_AIRLINES_FAIL:
-            return {
-                ...state,
-                airlinesLoading: false
-            };    
-        default:
-            return state;
+        case actionTypes.SET_AIRLINES_OFFSET_LIMIT: return setAirlinesOffsetLimit(state, action);
+        case actionTypes.SET_AIRLINES_PAGE: return setAirlinesPage(state, action);            
+        case actionTypes.FETCH_AIRLINES_START: return fetchAirlinesStart(state, action);            
+        case actionTypes.FETCH_AIRLINES_SUCCESS: return fetchAirlinesSuccess(state, action);            
+        case actionTypes.FETCH_AIRLINES_FAIL: return fetchAirlinesFail(state, action);
+        default: return state;
     }
 };
 
