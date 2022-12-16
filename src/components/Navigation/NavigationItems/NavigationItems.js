d3fb7ff309f84f49285a8df9f1c5f9e1c498fa54
Mladen <mladen56717@its.edu.rs>
2022-12-16 10:14:21 +0100

    Cutomer Navigation Bar Fixed

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react';
+import React, {useContext, useEffect} from 'react';
 import classes from './NavigationItems.module.css';
 import NavigationItem from './NavigationItem/NavigationItem';
 import {AuthContext} from '../../../context/auth-context';
@@ -6,43 +6,132 @@ import {AuthContext} from '../../../context/auth-context';
 
 
 
-const navigationItems = ( props ) => (
-    
-    
-    
+const NavigationItems = ( props ) => {
+    const authContext = useContext(AuthContext);
+    const authCheckState = authContext.authenticationCheckState;
+    let isAuthenticated = authContext.user.token !== null;
+    let isRole = authContext.user.role == "Admin" ;
+    let isParser = authContext.user.role == "Parser" && authContext.user.token !== null;
+    let isCustomer = authContext.user.role == "Customer"&& authContext.user.token !== null;
+
+    useEffect(() => {
+        authCheckState();
+      }, [authCheckState]);
+
+    if(!isAuthenticated)
+    {
+        return(
+            <ul className={classes.NavigationItems}>
+                <NavigationItem link="/auth">Log in</NavigationItem>
+            </ul>
+        )
+    }
+    if(isAuthenticated){
+        if(isCustomer){
+            return(<ul className={classes.NavigationItems}>
+                <NavigationItem link="/akrx">ACARS raw</NavigationItem>
+                <NavigationItem link="/acarsWithExtData">ACARS per Aircraft</NavigationItem>
+                <NavigationItem link="/adsb">ADSB messages</NavigationItem>
+                <NavigationItem link="/airports">Airports</NavigationItem>            
+                <NavigationItem link="/openstreetMap">Map</NavigationItem>   
+                <NavigationItem link="/logout">Log out</NavigationItem>         
+            </ul>)
+        }
+        if(isParser){
+            return(<ul className={classes.NavigationItems}>
+                <NavigationItem link="/akrx">ACARS raw</NavigationItem>
+                <NavigationItem link="/acarsWithExtData">ACARS per Aircraft</NavigationItem>
+                <NavigationItem link="/adsb">ADSB messages</NavigationItem>
+                <NavigationItem link="/airports">Airports</NavigationItem>
+                <NavigationItem link="/aircraft">Aircraft</NavigationItem>
+                <NavigationItem link="/airlines">Airlines</NavigationItem>
+                <NavigationItem link="/openstreetMap">Map</NavigationItem> 
+                <NavigationItem link="/statistics">Statistics</NavigationItem>
+                <NavigationItem link="/decoding">Decoder</NavigationItem>   
+                <NavigationItem link="/logout">Log out</NavigationItem>        
+            </ul>)
+        }
+        if(isRole){
+            return(<ul className={classes.NavigationItems}>
+                <NavigationItem link="/akrx">ACARS raw</NavigationItem>
+                <NavigationItem link="/acarsWithExtData">ACARS per Aircraft</NavigationItem>
+                <NavigationItem link="/adsb">ADSB messages</NavigationItem>
+                <NavigationItem link="/airports">Airports</NavigationItem>
+                <NavigationItem link="/aircraft">Aircraft</NavigationItem>
+                <NavigationItem link="/airlines">Airlines</NavigationItem>
+                <NavigationItem link="/openstreetMap">Map</NavigationItem> 
+                <NavigationItem link="/statistics">Statistics</NavigationItem>
+                {/* <NavigationItem link="/decoding">Decoder</NavigationItem>    */}
+                <NavigationItem link="/administrator">Administrator</NavigationItem>
+                <NavigationItem link="/logout">Log out</NavigationItem>        
+            </ul>)
+        }
+        else{
+            return(<ul className={classes.NavigationItems}>
+                <NavigationItem link="/akrx">ACARS raw</NavigationItem>
+                <NavigationItem link="/acarsWithExtData">ACARS per Aircraft</NavigationItem>
+                <NavigationItem link="/adsb">ADSB messages</NavigationItem>
+                <NavigationItem link="/airports">Airports</NavigationItem>
+                <NavigationItem link="/aircraft">Aircraft</NavigationItem>
+                <NavigationItem link="/airlines">Airlines</NavigationItem>
+                <NavigationItem link="/openstreetMap">Map</NavigationItem>                 
+                <NavigationItem link="/logout">Log out</NavigationItem>        
+            </ul>) 
+        }
+              
+    }
+    // if(props.isAuthenticated && !props.isCustomer){
+    //     return(<ul className={classes.NavigationItems}>
+    //         <NavigationItem link="/akrx">ACARS raw</NavigationItem>
+    //         <NavigationItem link="/acarsWithExtData">ACARS per Aircraft</NavigationItem>
+    //         <NavigationItem link="/adsb">ADSB messages</NavigationItem>
+    //         <NavigationItem link="/airports">Airports</NavigationItem>
+    //         <NavigationItem link="/aircraft">Aircraft</NavigationItem>
+    //         <NavigationItem link="/airlines">Airlines</NavigationItem>
+    //         <NavigationItem link="/openstreetMap">Map</NavigationItem>            
+    //     </ul>)
+    // }
+    // if(props.isAuthenticated && props.isCustomer){
+    //     return(<ul className={classes.NavigationItems}>
+    //         <NavigationItem link="/akrx">ACARS raw</NavigationItem>
+    //         <NavigationItem link="/acarsWithExtData">ACARS per Aircraft</NavigationItem>
+    //         <NavigationItem link="/adsb">ADSB messages</NavigationItem>
+    //         <NavigationItem link="/airports">Airports</NavigationItem>            
+    //         <NavigationItem link="/openstreetMap">Map</NavigationItem>            
+    //     </ul>)
+    // }
     // console.log("AUTH "+props.isAuthenticated),
     // console.log("PARSE "+props.isParser),
     // console.log("ROLE "+props.isRole),
    
-        <ul className={classes.NavigationItems}>      
+    //     <ul className={classes.NavigationItems}>      
+                
+    //         {(props.isAuthenticated ) ? <NavigationItem link="/akrx">ACARS raw</NavigationItem> : null}
+    //         {(props.isAuthenticated ) ? <NavigationItem link="/acarsWithExtData">ACARS per Aircraft</NavigationItem> : null}
+    //         {(props.isAuthenticated ) ? <NavigationItem link="/adsb">ADSB messages</NavigationItem> : null}
+                
+    //         {(props.isAuthenticated ) ? <NavigationItem link="/airports">Airports</NavigationItem> : null}       
+    //         {(props.isAuthenticated && !props.isCustomer) ? <NavigationItem link="/aircraft">Aircraft</NavigationItem> : null}
             
-        
-        {(props.isAuthenticated ) ? <NavigationItem link="/akrx">ACARS raw</NavigationItem> : null}
-        {(props.isAuthenticated ) ? <NavigationItem link="/acarsWithExtData">ACARS per Aircraft</NavigationItem> : null}
-        {(props.isAuthenticated ) ? <NavigationItem link="/adsb">ADSB messages</NavigationItem> : null}
-              
-        {(props.isAuthenticated ) ? <NavigationItem link="/airports">Airports</NavigationItem> : null}       
-         {(props.isAuthenticated && props.isParser || props.isAuthenticated && props.isRole || props.isAuthenticated && !props.isCustomer) ? <NavigationItem link="/aircraft">Aircraft</NavigationItem> : null}
-        
-         {(props.isAuthenticated && props.isParser || props.isAuthenticated && props.isRole || props.isAuthenticated && !props.isCustomer) ? <NavigationItem link="/airlines">Airlines</NavigationItem> : null}
-        
-        {(props.isAuthenticated) ? <NavigationItem link="/openstreetMap">Map</NavigationItem> : null}       
+    //         {(props.isAuthenticated && !props.isCustomer) ? <NavigationItem link="/airlines">Airlines</NavigationItem> : null}
+            
+    //         {(props.isAuthenticated) ? <NavigationItem link="/openstreetMap">Map</NavigationItem> : null}       
 
-        {(props.isAuthenticated && props.isParser) ? <NavigationItem link="/decoding">Decoder</NavigationItem> : null}
+    //         {(props.isAuthenticated && props.isParser) ? <NavigationItem link="/decoding">Decoder</NavigationItem> : null}
 
-        {(props.isAuthenticated && props.isParser || props.isAuthenticated && props.isRole || props.isAuthenticated && !props.isCustomer) ? <NavigationItem link="/statistics">Statistics</NavigationItem> : null}
-        
-        {(props.isAuthenticated && props.isRole) ? <NavigationItem link="/administrator">Administrator</NavigationItem> : null}
-        
-        
-        {!props.isAuthenticated
-            ? <NavigationItem link="/auth">Log in</NavigationItem>
-            : <NavigationItem link="/logout">Log out</NavigationItem>}
+    //         {(props.isAuthenticated && !props.isCustomer) ? <NavigationItem link="/statistics">Statistics</NavigationItem> : null}
+            
+    //         {(props.isAuthenticated && props.isRole) ? <NavigationItem link="/administrator">Administrator</NavigationItem> : null}
+            
+            
+    //         {!props.isAuthenticated
+    //             ? <NavigationItem link="/auth">Log in</NavigationItem>
+    //             : <NavigationItem link="/logout">Log out</NavigationItem>}
 
                      
-    </ul>
+    // </ul>
    
     
-)
+    }
 
-export default navigationItems;
+export default NavigationItems;
